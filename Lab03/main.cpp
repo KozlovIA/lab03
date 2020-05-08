1dd53a8cf93547f3aeccebfa1038b31789f2e772
Igor <iak@7bk.ru>
2020-05-08 19:36:17 +0300

    cURL EXAMPLE

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -7,9 +7,25 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
     curl_global_init(CURL_GLOBAL_ALL);
+    if(argc > 1)
+    {
+        CURL *curl = curl_easy_init();
+        if(curl)
+        {
+            CURLcode res;
+            curl_easy_setopt(curl, CURLOPT_URL, argv[1]);
+            res = curl_easy_perform(curl);
+            curl_easy_cleanup(curl);
+        }
+
+        for(int i=0; i < argc; i++)
+        {
+            cout << "argc[" << i << "]=" << argv[i] << '/n';
+        }
+    }
     // Ввод данных
     const auto input = read_input(cin, true);
     // Вывод данных
