2b162d7528c052ea6cd17c219519fbe39035bb94
Igor <iak@7bk.ru>
2020-05-08 18:03:55 +0300

    3.2.3 library installation

--- Lab03/main.cpp
+++ Lab03/main.cpp
@@ -3,10 +3,13 @@
 #include <string>
 #include "histogram.h"
 #include "svg.h"
+#include <curl/curl.h>
+
 using namespace std;
 
 int main()
 {
+    curl_global_init(CURL_GLOBAL_ALL);
     // Ввод данных
     const auto input = read_input(cin, true);
     // Вывод данных
